2ee014bb0054d266285e43b4126645187b73c472
Luis Ibarra <luise98cu@gmail.com>
2023-11-05 13:05:15 -0500

    Fixing compilation

--- rcci/clean.py
+++ rcci/clean.py
@@ -3,19 +3,26 @@ import re
 
 latex_file = Path(__file__).parent / "rcci_template.tex"
 
-latex_content = latex_file.read_text()
+latex_content = latex_file.read_text(encoding='utf8')
 
 subs = [
-    ("á",r"\'a"),
-    ("é",r"\'e"),
-    ("í",r"\'i"),
-    ("ó",r"\'o"),
-    ("ú",r"\'u"),
-    ("ü",r'\"u'),
-    ("ñ",r"\~n"),
+    ("á", r"\'a"),
+    ("é", r"\'e"),
+    ("í", r"\'i"),
+    ("ó", r"\'o"),
+    ("ú", r"\'u"),
+    ("ü", r'\"u'),
+    ("ñ", r"\~n"),
+    ("Á", r"\'A"),
+    ("É", r"\'E"),
+    ("Í", r"\'I"),
+    ("Ó", r"\'O"),
+    ("Ú", r"\'U"),
+    ("Ü", r'\"U'),
+    ("Ñ", r"\~N"),
 ]
 
 for find, replace in subs:
     latex_content = re.sub(find, replace, latex_content)
 
-latex_file.write_text(latex_content)
+latex_file.write_text(latex_content, encoding="utf8")
